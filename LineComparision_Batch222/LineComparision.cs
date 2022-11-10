d37b54f749a40abf23cda4cbe764bc57a917ac29
Aneesh <katta.aneesh597@gmail.com>
2022-11-10 18:50:59 +0530

    added welcome message

---
+++ LineComparision_Batch222/LineComparision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparision_Batch222
+{
+    class LineComparision
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to line comparision problem");
+        }
+    }
+}
